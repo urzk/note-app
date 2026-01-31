599a4603896f5a86d10ec19247070fafce449b74
urzk <shgerk@gmail.com>
2026-01-31 22:01:07 +0900

    api 実装

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,4 +1,6 @@
-import express, { Express, Request, Response } from "express";
+import express from "express";
+import { pool } from "./db";
+
 const app = express();
 const port = 3000;
 
@@ -8,4 +10,27 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+app.get("v1/users", async (req, res) => {
+  const updated_after =
+    typeof req.query.updated_after === "string"
+      ? new Date(req.query.updated_after)
+      : undefined;
+  console.log(updated_after);
+  try {
+    if (updated_after && !isNaN(updated_after.getTime())) {
+      const [rows] = await pool.query(
+        "SELECT * FROM notes WHERE updated_at > ?",
+        [updated_after],
+      );
+      res.json(rows);
+    } else {
+      const [rows] = await pool.query("SELECT * FROM notes");
+      res.json(rows);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err });
+  }
 });
