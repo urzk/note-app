1ad2683577f158d8bc0f64a2b6456d6189e1a57f
urzk <shgerk@gmail.com>
2026-03-04 14:44:38 +0900

    css adjustments2

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -4,6 +4,8 @@ import "./style.css";
 import { useFetchedServerNotesData, useFetchNotesData } from "./hooks/fetch";
 import MDEditor from "@uiw/react-md-editor";
 
+import { NoteListItem } from "./NoteListItem";
+
 function App() {
   const { data } = useFetchedServerNotesData();
   useFetchNotesData();
@@ -11,14 +13,20 @@ function App() {
   const [value, setValue] = useState<string | undefined>("");
   if (data) {
     return (
-      <div className="flex">
-        <div className="w-3xs">
-          {data.notes.map((note) => (
-            <div key={note.id}>{note.title}</div>
-          ))}
+      <div className="flex h-screen overflow-hidden">
+        <div className="w-3xs flex flex-col">
+          <ul className="flex-1 overflow-y-auto">
+            {data.notes.map((note) => (
+              <NoteListItem
+                id={note.id}
+                title={note.title}
+                updatedAt={note.updatedAt}
+              />
+            ))}
+          </ul>
         </div>
         <div className="grow">
-          <MDEditor height={1000} value={value} onChange={setValue} />
+          <MDEditor height={500} value={value} onChange={setValue} />
           {/* TODO: markdownパース処理をWeb Workerにやらせる */}
         </div>
       </div>
