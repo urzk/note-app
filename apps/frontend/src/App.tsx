7bf55a615254b4d8f4b3bc1ccbdbbc2b77d9a364
urzk <shgerk@gmail.com>
2026-03-04 12:16:34 +0900

    added react-md-editor

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./style.css";
 
 import { useFetchedServerNotesData, useFetchNotesData } from "./hooks/fetch";
+import MDEditor from "@uiw/react-md-editor";
 
 function App() {
   const { data } = useFetchedServerNotesData();
   useFetchNotesData();
   useEffect(() => console.log(data));
+  const [value, setValue] = useState<string | undefined>("");
   if (data) {
     return (
       <div className="flex">
@@ -15,7 +17,10 @@ function App() {
             <div key={note.id}>{note.title}</div>
           ))}
         </div>
-        <div className="grow">にゃーん</div>
+        <div className="grow">
+          <MDEditor height={1000} value={value} onChange={setValue} />
+          {/* TODO: markdownパース処理をWeb Workerにやらせる */}
+        </div>
       </div>
     );
   }
