d6a301e571d5ca0ac7ce6504a4337cadc35c406f
urzk <shgerk@gmail.com>
2026-03-05 13:40:02 +0900

    css adjustments

--- apps/frontend/src/NoteListItem.tsx
+++ apps/frontend/src/NoteListItem.tsx
@@ -8,12 +8,14 @@ export const NoteListItem = ({
   updatedAt: Date;
 }) => {
   return (
-    <li className="px-4 py-1" key={id}>
-      <div>
-        <h2>{title}</h2>
-      </div>
-      <div>
-        <small>2026/03/04</small>
+    <li className="px-3" key={id}>
+      <div className="px-3 py-1 rounded-md hover:bg-zinc-800 hover:shadow-lg">
+        <div>
+          <h2>{title}</h2>
+        </div>
+        <div>
+          <small>2026/03/04</small>
+        </div>
       </div>
     </li>
   );
