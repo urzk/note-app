4bc73925aba80dbc03c7d11e058faab1c84784c7
urzk <shgerk@gmail.com>
2026-03-17 17:26:57 +0900

    applied GFM & added @tailwindcss/typography

--- apps/frontend/src/worker/md2hast.worker.ts
+++ apps/frontend/src/worker/md2hast.worker.ts
@@ -1,11 +1,12 @@
 import { unified } from "unified";
 import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
 import remarkRehype from "remark-rehype";
 import { removePosition } from "unist-util-remove-position";
 
 onmessage = async (e: MessageEvent<{ id: number; md: string }>) => {
   const { id, md } = e.data;
-  const processor = unified().use(remarkParse).use(remarkRehype);
+  const processor = unified().use(remarkParse).use(remarkGfm).use(remarkRehype);
   const mdast = processor.parse(md);
   removePosition(mdast);
   const hast = await processor.run(mdast);
