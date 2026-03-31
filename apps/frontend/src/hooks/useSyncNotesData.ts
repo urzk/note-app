b5b4f3779bca4c7926409297f9c8799d49b01d44
urzk <shgerk@gmail.com>
2026-03-31 10:23:31 +0900

    set time stamps

--- apps/frontend/src/hooks/useSyncNotesData.ts
+++ apps/frontend/src/hooks/useSyncNotesData.ts
@@ -1,43 +1,50 @@
 import { useRef } from "react";
 import useSWR from "swr";
+
 import type { Note, NotesApi } from "@shared/types/note";
 import { mergeNotes } from "../utils/mergeNotes";
 import { syncFetcher } from "../utils/syncFetcher";
 
 export const useSyncNotesData = () => {
   const { data: notesSyncCurrent } = useSWR<NotesApi>("notes-synced", null);
-  const { data: notesLocalUpdated, mutate: notesLocalMutate } = useSWR<Note[]>(
+  const { data: notesLocalUpdates, mutate: notesLocalMutate } = useSWR<Note[]>(
     "notes-updated",
     null,
   );
-  const fulfilledSyncLocalTimeStampRef = useRef<{
-    start: number;
-  } | null>(null);
+  const postLocalTimeStampRef = useRef<number>(0);
+  const serverTimeRef = useRef<number>(0);
 
   useSWR<NotesApi>( // only process to update "notes-synced" in this app
     "notes-synced",
     async () => {
       const updatedAfter = notesSyncCurrent?.serverTime;
-      const syncStartTimeStamp = Date.now();
+      const hasUpdates = notesLocalUpdates && notesLocalUpdates.length > 0;
+      const startTimeStamp = hasUpdates ? notesLocalUpdates[0].updatedAt : 0;
+
       const notesSyncUpdates = await syncFetcher(
         updatedAfter,
-        [], // -> notesLocalUpdated,
+        [], // -> notesLocalUpdates,
       );
-      // console.log("data fetched!");
       const notesSyncNew = notesSyncCurrent
         ? mergeNotes(notesSyncCurrent, notesSyncUpdates)
         : notesSyncUpdates;
-      fulfilledSyncLocalTimeStampRef.current = {
-        start: syncStartTimeStamp,
-      };
+
+      const serverTime = notesSyncNew.serverTime;
+      if (serverTimeRef.current > serverTime) {
+        throw new Error("Not newest data!");
+      }
+      serverTimeRef.current = serverTime;
+      if (hasUpdates && postLocalTimeStampRef.current < startTimeStamp) {
+        postLocalTimeStampRef.current = startTimeStamp;
+      }
       return notesSyncNew;
     },
     {
       /*
       onSuccess: () => {
-        notesLocalMutate((notesLocalUpdated) => {
-          const timeStamp = fulfilledSyncLocalTimeStampRef?.current?.start ?? 0;
-          return notesLocalUpdated?.filter(
+        notesLocalMutate((notesLocalUpdates) => {
+          const timeStamp = postLocalTimeStampRef.current;
+          return notesLocalUpdates?.filter(
             (note) => note.updatedAt > timeStamp,
           );
         });
