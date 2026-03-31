6e202dfaf8004425360489262037483916caee33
urzk <shgerk@gmail.com>
2026-03-31 11:49:45 +0900

    separated getNotes

---
+++ apps/backend/src/getNotes.ts
@@ -0,0 +1,34 @@
+import type { RowDataPacket } from "mysql2";
+import type { Note, NotesApi } from "@shared/types/note.js";
+import { pool } from "./db.js";
+
+export interface NoteDB extends RowDataPacket {
+  id: number;
+  user_id: number | null; // TODO:
+  title: string;
+  content: string;
+  created_at: Date;
+  updated_at: Date;
+  is_deleted: number;
+}
+
+export const getNotes = async (updatedAfter: Date | undefined) => {
+  let query = "SELECT * FROM notes WHERE user_id IS NULL";
+  if (updatedAfter && !isNaN(updatedAfter.getTime())) {
+    query += " AND updated_at > ?";
+  }
+  query += " ORDER BY updated_at DESC";
+  const serverTime = Date.now();
+  const [notesDB] = await pool.query<NoteDB[]>(query, [updatedAfter]);
+  const notes: Note[] = notesDB.map(
+    ({ id, title, content, updated_at, is_deleted }) => ({
+      id,
+      title,
+      content,
+      updatedAt: updated_at.getTime(),
+      isDeleted: is_deleted == 1,
+    }),
+  );
+  const notesApiResponse: NotesApi = { serverTime, notes };
+  return notesApiResponse;
+};
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,19 +1,8 @@
 import express from "express";
 import type { Request, Response } from "express";
-import { pool } from "./db.js";
 import cors from "cors";
 import type { Note, NotesApi } from "@shared/types/note.js";
-import type { RowDataPacket } from "mysql2";
-
-export interface NoteDB extends RowDataPacket {
-  id: number;
-  user_id: number | null; // TODO:
-  title: string;
-  content: string;
-  created_at: Date;
-  updated_at: Date;
-  is_deleted: number;
-}
+import { getNotes } from "./getNotes.js";
 
 const app = express();
 const port = 3000;
@@ -24,10 +13,6 @@ app.use(
     credentials: true,
   }),
 );
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
@@ -44,24 +29,8 @@ app.get(
       typeof req.query.updatedAfter === "string"
         ? new Date(Number(req.query.updatedAfter))
         : undefined;
-    const serverTime = Date.now();
     try {
-      let query = "SELECT * FROM notes WHERE user_id IS NULL";
-      if (updatedAfter && !isNaN(updatedAfter.getTime())) {
-        query += " AND updated_at > ?";
-      }
-      query += " ORDER BY updated_at DESC";
-      const [notesDB] = await pool.query<NoteDB[]>(query, [updatedAfter]);
-      const notes: Note[] = notesDB.map(
-        ({ id, title, content, updated_at, is_deleted }) => ({
-          id,
-          title,
-          content,
-          updatedAt: updated_at.getTime(),
-          isDeleted: is_deleted == 1,
-        }),
-      );
-      const notesApiResponse: NotesApi = { serverTime, notes };
+      const notesApiResponse = await getNotes(updatedAfter);
       console.log(notesApiResponse);
       res.json(notesApiResponse);
     } catch (err) {
