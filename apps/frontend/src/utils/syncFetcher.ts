966f1b9cfec66db207bc9f08c0a34a03a9c8d258
urzk <shgerk@gmail.com>
2026-04-01 11:22:28 +0900

    modified putPath

--- apps/frontend/src/utils/syncFetcher.ts
+++ apps/frontend/src/utils/syncFetcher.ts
@@ -4,9 +4,9 @@ import type { Note, NotesApi } from "@shared/types/note";
 const origin = "http://localhost:3000";
 
 const getPath = "/v1/notes";
-const postPath = "/v1/notes/sync";
+const putPath = "/v1/notes-sync";
 const getUrl = origin + getPath;
-const postUrl = origin + postPath;
+const putUrl = origin + putPath;
 
 export const syncFetcher = async (
   updatedAfter: number | undefined,
@@ -14,7 +14,7 @@ export const syncFetcher = async (
 ): Promise<NotesApi> => {
   try {
     if (updatedNotes && updatedNotes.length > 0) {
-      const res = await axios.put<NotesApi>(postUrl, {
+      const res = await axios.put<NotesApi>(putUrl, {
         updatedAfter,
         updatedNotes,
       });
