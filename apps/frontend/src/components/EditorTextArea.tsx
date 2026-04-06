baf926c91aacff8e37b7699d97ad4924f937a600
urzk <shgerk@gmail.com>
2026-04-06 10:57:23 +0900

    set text when no note selected

--- apps/frontend/src/components/EditorTextArea.tsx
+++ apps/frontend/src/components/EditorTextArea.tsx
@@ -56,10 +56,10 @@ export const EditorTextArea = ({
 
   return (
     <textarea
-      className="flex-1 view resize-none outline-0 disabled:bg-zinc-950"
+      className="flex-1 view resize-none outline-0 disabled:text-zinc-400"
       disabled={!note}
       ref={textareaRef}
-      value={note?.content ?? ""}
+      value={note ? note.content : "No note selected"}
       onChange={(e) => {
         const content = e.target.value;
         if (note) {
