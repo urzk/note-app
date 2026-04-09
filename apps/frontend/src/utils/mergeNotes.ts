996dd1360a114ef72b1e0e877d7b80e0befc2fca
urzk <shgerk@gmail.com>
2026-04-09 14:00:46 +0900

    throw error if time paradox

--- apps/frontend/src/utils/mergeNotes.ts
+++ apps/frontend/src/utils/mergeNotes.ts
@@ -6,6 +6,11 @@ export const mergeNotes = (current: Note[], updated: Note[]): Note[] => {
   } else if (updated.length == 0) {
     return current;
   }
+  if (updated[updated.length - 1].updatedAt < current[0].updatedAt) {
+    const err = new Error("time paradox");
+    console.error(err);
+    throw err;
+  }
   const updatedIds = new Set<number>();
   updated.forEach((note) => {
     updatedIds.add(note.id);
