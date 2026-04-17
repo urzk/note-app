8957b79488790cd595248c8b7a557bcf1fbe7ad6
urzk <shgerk@gmail.com>
2026-04-17 10:23:01 +0900

    import saved synced data

--- apps/frontend/src/hooks/useSyncNotesData.ts
+++ apps/frontend/src/hooks/useSyncNotesData.ts
@@ -1,7 +1,9 @@
 import { useRef } from "react";
 import useSWR, { mutate } from "swr";
+import useSWRImmutable from "swr/immutable";
 
 import db from "src/lib/db/clientDB";
+import localFetcher from "src/utils/localFetcher";
 import type { Note } from "@shared/types/note";
 import { mergeNotes } from "../utils/mergeNotes";
 import { getFetcher, putFetcher } from "../utils/fetcher";
@@ -11,20 +13,25 @@ import type {
 } from "@shared/types/note";
 
 export const useSyncNotesData = () => {
-  const { data: notesSyncedCurrent } = useSWR<Note[]>("notes-synced", null);
-  const { data: notesLocalUpdates } = useSWR<Note[]>("notes-updated", null);
+  const { data: notesSynced, mutate: mutateNotesSynced } = useSWRImmutable<
+    Note[]
+  >("notes-synced", () => localFetcher("synced"), { refreshWhenOffline: true });
+  const { data: notesUpdated, mutate: mutateNotesUpdated } = useSWR<Note[]>(
+    "notes-updated",
+    null,
+  );
   const putLocalTimeStampRef = useRef<number>(0);
 
-  const hasSynced = notesSyncedCurrent && notesSyncedCurrent.length > 0;
-  const hasLocalUpdates = notesLocalUpdates && notesLocalUpdates.length > 0;
-  const updatedAfter = hasSynced ? notesSyncedCurrent[0].updatedAt : undefined;
-  const startTimeStamp = hasLocalUpdates ? notesLocalUpdates[0].updatedAt : 0;
+  const hasSynced = notesSynced && notesSynced.length > 0;
+  const hasUpdated = notesUpdated && notesUpdated.length > 0;
+  const updatedAfter = hasSynced ? notesSynced[0].updatedAt : undefined;
+  const startTimeStamp = hasUpdated ? notesUpdated[0].updatedAt : 0;
 
   useSWR<NotesApiResponse | NotesSyncApiResponse>( // only process to update "notes-synced" in this app
     "notes-sync",
     async () =>
-      hasLocalUpdates
-        ? await putFetcher(updatedAfter, notesLocalUpdates)
+      hasUpdated
+        ? await putFetcher(updatedAfter, notesUpdated)
         : await getFetcher(updatedAfter),
     {
       onSuccess: async (apiResponse) => {
@@ -33,26 +40,26 @@ export const useSyncNotesData = () => {
           updateError = apiResponse.updateError;
           console.error(updateError);
         }
-        const { serverTime, notes: notesUpdated } = apiResponse;
+        const { serverTime, notes: notesFromServer } = apiResponse;
 
         mutate<number>("last-synced-time", serverTime);
 
-        await db.synced.bulkPut(notesUpdated);
+        await db.synced.bulkPut(notesFromServer);
 
-        const notesSyncedNew = notesSyncedCurrent
-          ? mergeNotes(notesSyncedCurrent, notesUpdated)
-          : notesUpdated;
-        mutate<Note[]>("notes-synced", notesSyncedNew);
+        const notesSyncedNew = notesSynced
+          ? mergeNotes(notesSynced, notesFromServer)
+          : notesFromServer;
+        mutateNotesSynced<Note[]>(notesSyncedNew, false);
 
         if (
-          hasLocalUpdates &&
+          hasUpdated &&
           !updateError &&
           putLocalTimeStampRef.current < startTimeStamp
         ) {
           putLocalTimeStampRef.current = startTimeStamp;
         }
 
-        mutate<Note[]>("notes-updated", async (notesLocalUpdates) => {
+        mutateNotesUpdated<Note[]>(async (notesLocalUpdates) => {
           const timeStamp = putLocalTimeStampRef.current;
           const unSyncedNotes = notesLocalUpdates?.filter(
             (note) => note.updatedAt > timeStamp,
