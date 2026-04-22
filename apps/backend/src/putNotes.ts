5458d0d9fdeba13506055877729527a16343b786
urzk <shgerk@gmail.com>
2026-04-22 11:33:24 +0900

    put first old data

--- apps/backend/src/putNotes.ts
+++ apps/backend/src/putNotes.ts
@@ -3,8 +3,10 @@ import { putNoteQuery } from "./putNoteQuery.js";
 import type { PutResponse } from "@shared/types/note.js";
 
 export const putNotes = async (notes: Note[]) => {
-  const promises = notes.map(async (note, index): Promise<PutResponse> => {
-    return await putNoteQuery(note);
-  });
+  const promises = notes
+    .map(async (note): Promise<PutResponse> => {
+      return await putNoteQuery(note);
+    })
+    .reverse();
   return await Promise.all(promises);
 };
